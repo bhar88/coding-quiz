3876a74508b69608acf7053eb186e4855fadad90
barry hardacre <baz.hardacre@gmail.com>
2023-01-13 15:43:30 +0000

    first commit

---
+++ starter/questions.js
@@ -0,0 +1,61 @@
+// Barry Hardacre challenge coding quiz
+// var with array and object for questions
+
+//declared variables
+
+//working code
+
+
+// seconds left is 15 seconds per question:
+
+// holds interval time
+
+//holds penalty time
+
+//creates new element
+
+
+//triggers timer on button, shows user a display on the screen
+
+   //checking zero
+
+
+//renders questions and choices to page:
+
+//clears existing data
+
+//for loops to loop through all info inarray
+
+//append question title only
+
+//new for each for question choices
+
+
+
+//event to compare chopices with answers
+
+     //correct condition
+
+     //correct condition
+
+     //will deduct 5 sweconds off secondsLeft for wrong answers
+
+//question index determines number question user is on
+   //all done will apend last page with user stats
+
+  // all done will appaend last page
+
+  //heading
+
+  //paragraph
+
+  //calculates time remaining and replaces it with score
+   //label
+
+   //input
+   
+   //submit
+
+   //event listener to capture initials and local storage fro initials and score
+
+   
